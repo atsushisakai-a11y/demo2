628e96e2a5e49b56e015e7b267328fa5ef693b04
atsushisakai-a11y <atsushi.sakai@gmail.com>
2026-02-23 16:29:22 +0100

    Create datamart_test_google_poi.sql

---
+++ models/datamart/datamart_test_google_poi.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table', schema='datamart', alias='datamart_test_google_poi') }}
+
+SELECT
+  CAST(DATE_TRUNC(f.fetched_at, MONTH) AS DATE) AS fetched_at,
+  COUNT(DISTINCT d.place_id) AS places,
+  ROUND(AVG(f.rating), 2) AS rating,
+  ROUND(AVG(f.user_ratings_total), 2) AS user_ratings_total
+FROM {{ ref('dim_test_google_poi') }} AS d
+INNER JOIN {{ ref('fact_test_google_poi') }} AS f
+  ON f.place_id = d.place_id
+GROUP BY 1
+ORDER BY 1
