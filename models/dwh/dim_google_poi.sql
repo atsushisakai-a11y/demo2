68e70a75da9d48cbee4eccfb02270911a2ac8a6f
atsushisakai-a11y <atsushi.sakai@gmail.com>
2026-02-26 17:24:52 +0100

    Update dim_google_poi.sql

--- models/dwh/dim_google_poi.sql
+++ models/dwh/dim_google_poi.sql
@@ -1,13 +1,36 @@
-{{ config(materialized='table', schema='dwh', alias='dim_google_poi') }}
+{{ config(
+    materialized='table',
+    schema='dwh',
+    alias='dim_google_poi'
+) }}
+
+WITH deduplicated AS (
+
+    SELECT
+        s.place_id,
+        s.name,
+        s.address,
+        s.primary_type,
+        s.lat,
+        s.lng,
+        s.google_maps_url,
+        ROW_NUMBER() OVER (
+            PARTITION BY s.place_id
+            ORDER BY s.fetched_at DESC
+        ) AS rn
+
+    FROM {{ ref('staging_google_poi') }} AS s
+
+)
 
 SELECT
-  s.place_id,
-  s.name,
-  s.address,
-  s.primary_type,
-  s.lat,
-  s.lng,
-  s.google_maps_url
-FROM `eneco-488308.staging.staging_google_poi` AS s
-GROUP BY ALL
-ORDER BY 1, 2, 3
+    place_id,
+    name,
+    address,
+    primary_type,
+    lat,
+    lng,
+    google_maps_url
+
+FROM deduplicated
+WHERE rn = 1
