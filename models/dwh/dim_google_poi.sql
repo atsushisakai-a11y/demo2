2c2641bef106e9f9a1d0d66a3390f3c6995a4087
atsushisakai-a11y <atsushi.sakai@gmail.com>
2026-02-26 16:36:33 +0100

    Rename table alias and update source table

---
+++ models/dwh/dim_google_poi.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table', schema='dwh', alias='dim_google_poi') }}
+
+SELECT
+  s.place_id,
+  s.name,
+  s.address,
+  s.lat,
+  s.lng,
+  s.google_maps_url
+FROM `eneco-488308.staging.staging_google_poi` AS s
+GROUP BY ALL
+ORDER BY 1, 2, 3
